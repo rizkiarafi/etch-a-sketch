5bada6bbcddeaa1a7d0612c0e8a3bee70606d77e
rizkiarafi <130366702+rizkiarafi@users.noreply.github.com>
2026-03-04 19:13:12 +0700

    Write logic to make the square size dynamic

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+const gridContainer = document.querySelector("#container");
+
 const CONTAINER_SIZE = 800;
 const gridSize = 20;
 const squareSize = CONTAINER_SIZE / gridSize;
+
+for (let i = 0; i < Math.pow(gridSize, 2); i++) {
+  gridContainer.appendChild(newGridSquare());
+}
+
+function newGridSquare() {
+  const newSquare = document.createElement("div");
+
+  newSquare.style.width = squareSize + "px";
+  newSquare.style.height = squareSize + "px";
+  newSquare.classList.add("square");
+
+  return newSquare;
+}
